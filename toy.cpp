a19e0173d467f297560d10d6b828fc0d13814f33
charonxin <pluto@liii.pro>
2023-10-10 20:49:21 +0800

    update P 2

---
+++ toy.cpp
@@ -0,0 +1,199 @@
+
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+// Lexer
+
+enum Token {
+  tok_eof = -1,
+  
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5
+};
+
+static std::string IdentifierStr;
+static double NumVal;
+
+static int gettok() {
+  static int LastChar = ' ';
+
+  while (isspace(LastChar))
+    LastChar = getchar();
+  
+  if (isalpha(LastChar)) {  // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum(LastChar = getchar()))
+      IdentifierStr += LastChar;
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  if (isdigit(LastChar) || LastChar == '.') {
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+    NumVal = strtod(NumStr.c_str(), nullptr);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    do
+      LastChar = getchar();
+    while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF) 
+      return gettok(); 
+  }
+
+  if (LastChar == EOF)
+    return tok_eof;
+  
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
+
+
+// Abstract Syntax Tree (aka Parse Tree)
+
+namespace {
+
+class ExprAST {
+public:
+  virtual ~ExprAST() {}
+};
+
+class NumberExprAST : public ExprAST {
+  double Val;
+public:
+  NumberExprAST(double Val) : Val(Val) {}
+};
+
+class VariableExprAST : public ExprAST {
+  std::string Name;
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+public:
+  BinaryExprAST(char Op,std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+};
+
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+public:
+  PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+    : Name(Name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+    : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+auto LHS = std::make_unique<VariableExprAST>("x");
+auto RHS = std::make_unique<VariableExprAST>("y");
+auto Result = std::make_unique<BinaryExprAST>('+', std::move(LHS),
+                                              std::move(RHS));
+
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() { return CurTok = gettok(); }
+
+/// @brief LogError* - These are little helper functions for error handling.
+/// @param Str 
+/// @return 
+std::unique_ptr<ExprAST> LogError(const char *Str) {  // void?
+  fprintf(stderr, "Error: %s\n", Str);
+  return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str) {
+  LogError(Str);
+  return nullptr;
+}
+
+static std::unique_ptr<ExprAST> ParseExpression();
+
+/// @brief  numberexpr ::= number
+/// @return 
+static std::unique_ptr<ExprAST> ParseNumberExpr() {
+  auto Result = std::make_unique<NumberExprAST>(NumVal);
+  getNextToken();
+  return std::move(Result);
+}
+
+
+/// @brief parenexpr ::= '(' expression ')'
+/// @return 
+static std::unique_ptr<ExprAST> ParseParenExpr() {
+  getNextToken();
+  auto V = ParseNumberExpr();
+  if (!V) return nullptr;
+  if (CurTok != ')') return LogError("expected ')'");
+
+  getNextToken();
+  return V;
+}
+
+static std::unique_ptr<ExprAST> ParseIdenttifierExpr() {
+  std::string IdName = IdentifierStr;
+  getNextToken();
+  if (CurTok != '(') 
+    return std::make_unique<VariableExprAST>(IdName);
+  
+  getNextToken();
+  std::vector<std::unique_ptr<ExprAST>> Args;
+  if (CurTok != ')') {
+    while (true)
+    {
+      if (auto Arg = ParseExpression())
+    }
+    
+  }
+}
+
+static std::map<char,int> BinoPrecedence;
+
+
+
+}
